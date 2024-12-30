543d5e5dc01d24951c5ff7aac25585cd24283a51
Tiago Ceccon <ceccon.t@hotmail.com>
2024-12-29 21:54:42 -0300

    Remove redundant method

--- Chargen/src/main/java/dev/ceccon/gui/GUISession.java
+++ Chargen/src/main/java/dev/ceccon/gui/GUISession.java
@@ -35,7 +35,7 @@ public class GUISession {
                         Do not output any disclaimer or explanation, just the biography for the character.
                         """
         );
-        String messageContent = textDescription(userCharacter) + "\n\nWrite a suitable short biography for this character.";
+        String messageContent = userCharacter.getTextualDescription() + "\n\nWrite a suitable short biography for this character.";
         chat.addMessage(
                 "user",
                 LLMSanitizer.sanitizeForChat(messageContent)
@@ -44,26 +44,6 @@ public class GUISession {
         Message response = llmClient.getBioStreaming(chat, tokenConsumer, onFinish);
 
         return LLMSanitizer.sanitizeLLMSpecialTokens(response.content());
-    }
-
-    private String textDescription(FantasyCharacter c) {
-        StringBuilder sb = new StringBuilder();
-
-        sb.append("Name: "); sb.append(c.getName()); sb.append("\n");
-        sb.append("Race: "); sb.append(c.getRace()); sb.append("\n");
-        sb.append("Sex: "); sb.append(c.getSex()); sb.append("\n");
-        sb.append("Class: "); sb.append(c.getCharacterClass()); sb.append("\n");
-        sb.append("Alignment: "); sb.append(c.getAlignment()); sb.append("\n");
-        sb.append("\n");
-
-        sb.append("Strength: "); sb.append(c.getStrength()); sb.append("\n");
-        sb.append("Dexterity: "); sb.append(c.getDexterity()); sb.append("\n");
-        sb.append("Constitution: "); sb.append(c.getConstitution()); sb.append("\n");
-        sb.append("Intelligence: "); sb.append(c.getIntelligence()); sb.append("\n");
-        sb.append("Wisdom: "); sb.append(c.getWisdom()); sb.append("\n");
-        sb.append("Charisma: "); sb.append(c.getCharisma()); sb.append("\n");
-
-        return sb.toString();
     }
 
     public byte[] createAvatar(FantasyCharacter character) throws IOException {
